1b6b45234b4c06ea915ef46f0ec821f9f35f18c8
Markus M. May <160079+triplem@users.noreply.github.com>
2026-06-06 00:40:24 +0200

    feat(catalog): #37 implement structured JSON logging with traceId propagation

--- kbeatz-catalog/src/main/kotlin/org/javafreedom/kbeatz/catalog/Application.kt
+++ kbeatz-catalog/src/main/kotlin/org/javafreedom/kbeatz/catalog/Application.kt
@@ -3,15 +3,17 @@ package org.javafreedom.kbeatz.catalog
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
+import org.javafreedom.kbeatz.catalog.plugins.configureLogging
+import org.javafreedom.kbeatz.catalog.plugins.configureRouting
 import org.javafreedom.kbeatz.catalog.plugins.configureSerialization
 import org.javafreedom.kbeatz.catalog.plugins.configureStatusPages
-import org.javafreedom.kbeatz.catalog.plugins.configureRouting
 
 fun main() {
     embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
+    configureLogging()
     configureSerialization()
     configureStatusPages()
     configureRouting()
